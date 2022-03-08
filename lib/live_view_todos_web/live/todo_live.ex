1eef2928f04de11b231e580d561afb120819307b
ulusoyemre <emre@kekik.com.tr>
2022-03-08 10:41:54 +0300

    Generate interfaces, add live module.

---
+++ lib/live_view_todos_web/live/todo_live.ex
@@ -0,0 +1,20 @@
+defmodule LiveViewTodosWeb.TodoLive do 
+    use LiveViewTodosWeb, :live_view
+    alias LiveViewTodos.Todos
+
+    def mount(_params, _session, socket) do
+        {:ok, assign(socket, todos: Todos.list_todos())}
+    end
+
+    def handle_event("add", %{"todo" => todo}, socket) do
+        Todos.create_todo(todo)
+
+        {:noreply, fetch(socket)}
+    end
+
+
+    defp fetch(socket) do
+        assign(socket, todos: Todos.list_todos())
+    end
+
+end
